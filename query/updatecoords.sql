748c244d0e3c863602d4855fd8ed07acf06e9469
Karl Cheng <qantas94heavy@gmail.com>
2015-04-19 08:30:56 +0800

    Teste

--- query/updatecoords.sql
+++ query/updatecoords.sql
@@ -3,7 +3,7 @@ UPDATE players
 SET (aircraft, latitude, longitude, altitude, heading, tilt, roll, lastUpdate) =
     ($1, $2::double precision, $3::double precision, $4::double precision,
      $5::double precision, $6::double precision, $7::double precision, $8::bigint)
-WHERE accountID = $9::int
+WHERE accountID = $9::int;
 INSERT INTO players (aircraft, latitude, longitude, altitude, heading, tilt, roll, lastUpdate) SELECT
     ($1, $2::double precision, $3::double precision, $4::double precision,
      $5::double precision, $6::double precision, $7::double precision, $8::bigint)
