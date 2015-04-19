46ff0ec5c65d77f6048907425be91513ac4ad0a3
Karl Cheng <qantas94heavy@gmail.com>
2015-04-19 08:35:42 +0800

    JFJkejfklef

--- query/updatecoords.sql
+++ query/updatecoords.sql
@@ -3,8 +3,4 @@ UPDATE players
 SET (aircraft, latitude, longitude, altitude, heading, tilt, roll, lastUpdate) =
     ($1, $2::double precision, $3::double precision, $4::double precision,
      $5::double precision, $6::double precision, $7::double precision, $8::bigint)
-WHERE accountID = $9::int;
-INSERT INTO players (aircraft, latitude, longitude, altitude, heading, tilt, roll, lastUpdate) SELECT
-    ($1, $2::double precision, $3::double precision, $4::double precision,
-     $5::double precision, $6::double precision, $7::double precision, $8::bigint)
-      WHERE NOT EXISTS (SELECT * FROM players WHERE accountID = $9::int)
+WHERE accountID = $9::int
