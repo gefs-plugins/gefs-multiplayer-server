b51ef4f564b53f5c9fa61bc13695bf90977fb21b
Karl Cheng <qantas94heavy@gmail.com>
2015-04-19 08:27:22 +0800

    Yeeklfjs

--- query/updatecoords.sql
+++ query/updatecoords.sql
@@ -1,5 +1,10 @@
+
 UPDATE players
 SET (aircraft, latitude, longitude, altitude, heading, tilt, roll, lastUpdate) =
     ($1, $2::double precision, $3::double precision, $4::double precision,
      $5::double precision, $6::double precision, $7::double precision, $8::bigint)
 WHERE accountID = $9::int
+INSERT INTO players (aircraft, latitude, longitude, altitude, heading, tilt, roll, lastUpdate) SELECT
+    ($1, $2::double precision, $3::double precision, $4::double precision,
+     $5::double precision, $6::double precision, $7::double precision, $8::bigint)
+      WHERE NOT EXISTS (SELECT * FROM TABLE WHERE accountID = $9::int)
