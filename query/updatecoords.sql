d0c47ae2f268f67fd365ec5af43be4869b19efdb
Karl Cheng <qantas94heavy@gmail.com>
2015-04-19 08:31:55 +0800

    Tests

--- query/updatecoords.sql
+++ query/updatecoords.sql
@@ -7,4 +7,4 @@ WHERE accountID = $9::int;
 INSERT INTO players (aircraft, latitude, longitude, altitude, heading, tilt, roll, lastUpdate) SELECT
     ($1, $2::double precision, $3::double precision, $4::double precision,
      $5::double precision, $6::double precision, $7::double precision, $8::bigint)
-      WHERE NOT EXISTS (SELECT * FROM TABLE WHERE accountID = $9::int)
+      WHERE NOT EXISTS (SELECT * FROM players WHERE accountID = $9::int)
